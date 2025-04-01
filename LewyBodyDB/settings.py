fd429b01defde24cee920a481111005938c5ce26
marswilliams <ng526p@gmail.com>
2025-04-01 01:43:04 -0400

    Register models with admin.py and update urls

--- LewyBodyDB/settings.py
+++ LewyBodyDB/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.amazonaws.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Patient, Scan, Image, Annotation
 
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(Scan)
+admin.site.register(Image)
+admin.site.register(Annotation)
