a88f25bcba1f4dab993bdebb84f04cf24fbc613c
marswilliams <ng526p@gmail.com>
2025-04-01 14:19:49 -0400

    Move storagePath from Scan Model to Image model

--- application/models.py
+++ application/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Patient(models.Model):
-    patient_id = models.IntegerField(primary_key=True)
-    age = models.IntegerField()
-    sex = models.CharField(max_length=2)
+    patient_id = models.CharField(primary_key=True, max_length=50)
+    age = models.IntegerField(blank=True, null=True)
+    sex = models.CharField(max_length=2, blank=True, null=True)
     diagnosis = models.CharField(max_length=25)
     disease_stage = models.CharField(max_length=5, blank=True, null=True)
     enrollment_date = models.DateField(blank=True, null=True)
@@ -12,26 +12,26 @@ class Patient(models.Model):
         return str(self.patient_id)
 
 class Scan(models.Model):
-    scan_id = models.IntegerField(primary_key=True)
+    scan_id = models.CharField(primary_key=True, max_length=50)
     patient_id = models.ForeignKey(Patient, on_delete=models.CASCADE)
     scan_type = models.CharField(max_length=25)
-    scan_date = models.DateField()
+    scan_date = models.DateField(blank=True, null=True)
     scan_location = models.CharField(max_length=25, blank=True, null=True)
-    storage_path = models.CharField(max_length=90)
 
     def __str__(self):
         return str(self.scan_id)
 
 class Image(models.Model):
-    image_id = models.IntegerField(primary_key=True)
+    image_id = models.CharField( max_length=50)
     scan_id = models.ForeignKey(Scan, on_delete=models.CASCADE)
     image_type = models.CharField(max_length=25)
+    storage_path = models.CharField(max_length=90, default="")
 
     def __str__(self):
         return str(self.image_id)
 
 class Annotation(models.Model):
-    annotation_id = models.IntegerField(primary_key=True)
+    annotation_id = models.CharField(primary_key=True, max_length=50)
     image_id = models.ForeignKey(Image, on_delete=models.CASCADE)
     label = models.CharField(max_length=25)
 
