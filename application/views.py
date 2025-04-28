0507b4efd39ee077d520ff9111143e74a51a1cd1
marswilliams <ng526p@gmail.com>
2025-04-28 01:03:23 -0400

    Remove logger for debugging

--- application/views.py
+++ application/views.py
@@ -14,7 +14,6 @@ def search_landing(request):
     return render(request, 'search_landing.html')
 
 def search_results(request):
-    logger = logging.getLogger('django')
     def get_queryset():
         queryFilter = request.GET.get("filter")
         queryString = request.GET.get("q")
@@ -50,16 +49,13 @@ def search_results(request):
     return render(request, "search_results.html", {'scans': get_queryset(), 'request': request})
 
 def scan_page(request, scan_id):
-    logger = logging.getLogger('django')
     scan = Scan.objects.get(scan_id=scan_id)
     images = Image.objects.filter(scan_id_id=scan_id)
     return render(request, "scan.html", {'scan': scan, 'images': images})
 
 def download_zipfile(request, scan_id):
-    logger = logging.getLogger('django')
     images = Image.objects.filter(scan_id_id=scan_id)
     imageList = [image.image_url for image in images]
-    logger.info(imageList)
     byte_data = io.BytesIO()
     zip_name = scan_id + ".zip"
 
