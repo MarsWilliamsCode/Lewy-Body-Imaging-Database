be204d2ef45baf5ee34996814fe9ca3dd3e5609d
marswilliams <ng526p@gmail.com>
2025-04-28 02:59:56 -0400

    Add pagination to search results

--- application/views.py
+++ application/views.py
@@ -4,10 +4,11 @@ import re
 import zipfile
 
 from django.conf import settings
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Scan, Image
-from django.db.models import Q
+from django.db.models import Q, Count
 import logging
 
 def search_landing(request):
@@ -18,35 +19,47 @@ def search_results(request):
         queryFilter = request.GET.get("filter")
         queryString = request.GET.get("q")
         queryList = re.split(r',\s*', queryString)
+        scans_with_images = Scan.objects.annotate(num_images=Count('image'))
+        scans_with_images = scans_with_images.filter(num_images__gt=0)
 
         if queryFilter == 'All':
-            scan_list = Scan.objects.filter(patient_id=-1)
+            scan_list = scans_with_images.filter(patient_id=-1)
             for query in queryList:
-                scan_list = scan_list.union(Scan.objects.filter(
-                Q(scan_type__icontains=query) | Q(patient_id__diagnosis__icontains=query) | Q(patient_id__patient_id__icontains=query)
+                scan_list = scan_list.union(scans_with_images.filter(
+                    (Q(scan_type__icontains=query) | Q(patient_id__diagnosis__icontains=query) | Q(patient_id__patient_id__icontains=query))
                 ))
 
         elif queryFilter == 'Disease':
-            scan_list = Scan.objects.filter(patient_id=-1)
+            scan_list = scans_with_images.filter(patient_id=-1)
             for query in queryList:
-                scan_list = scan_list.union(Scan.objects.filter(
+                scan_list = scan_list.union(scans_with_images.filter(
                 Q(patient_id__diagnosis__icontains=query)
                 ))
 
         elif queryFilter == 'Modality':
-            scan_list = Scan.objects.filter(patient_id=-1)
+            scan_list = scans_with_images.filter(patient_id=-1)
             for query in queryList:
-                scan_list = scan_list.union(Scan.objects.filter(
+                scan_list = scan_list.union(scans_with_images.filter(
                     Q(scan_type__icontains=query)
                 ))
         elif queryFilter == 'Stage':
-            scan_list = Scan.objects.filter(patient_id=-1)
+            scan_list = scans_with_images.filter(patient_id=-1)
             for query in queryList:
-                scan_list = scan_list.union(Scan.objects.filter(
+                scan_list = scan_list.union(scans_with_images.filter(
                     Q(patient_id__disease_stage__icontains=query)
                 ))
         return scan_list
-    return render(request, "search_results.html", {'scans': get_queryset(), 'request': request})
+    scan_list = get_queryset()
+
+    p = Paginator(scan_list, 25)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = p.page(1)
+    except EmptyPage:
+        page_obj = p.page(p.num_pages)
+    return render(request, "search_results.html", {'page_obj': page_obj, 'request': request})
 
 def scan_page(request, scan_id):
     scan = Scan.objects.get(scan_id=scan_id)
